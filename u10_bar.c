3489c67b73771f16b30005a1dd33aad1dcedc23f
kucera <kucera.david2002@gmail.com>
2023-12-08 17:45:34 +0100

    added u10_bar

---
+++ u10_bar.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+typedef struct pult {
+    bool pult;
+    pthread_mutex_t mutex;
+    pthread_cond_t odober;
+    pthread_cond_t pridaj;
+} PULT;
+
+typedef struct zakaznik {
+    signed char cas_prichodu;
+    unsigned int id;
+    PULT * pult;
+} ZAKAZNIK;
+
+typedef struct barman {
+    PULT * pult;
+    unsigned int pocet_drinkov;
+} BARMAN;
+
+void * barman_fun(void * arg) {
+    BARMAN * data = arg;
+    printf("Barman zacina sichtu! Bude robit %u drinkov!\n",data->pocet_drinkov);
+    for (int i = 1; i <= data->pocet_drinkov; ++i) {
+        printf("Pripravujem %d. drink typu",i);
+        if(rand()%2) {
+            printf(" 1 s cakanim 2s\n");
+            sleep(2);
+        } else {
+            printf(" 2 s cakanim 1s\n");
+            sleep(1);
+        }
+        pthread_mutex_lock(&data->pult->mutex);
+        while(data->pult->pult) {
+            pthread_cond_wait(&data->pult->pridaj,&data->pult->mutex);
+        }
+        data->pult->pult=true;
+        pthread_cond_signal(&data->pult->odober);
+        pthread_mutex_unlock(&data->pult->mutex);
+    }
+    printf("Barman skoncil sichtu!\n");
+    return NULL;
+}
+void * zakaznik_fun(void * arg) {
+    ZAKAZNIK * data = arg;
+    printf("Zakaznik %u ide do baru! Bude mu to trvat %hhd sekund!\n",data->id,data->cas_prichodu);
+    sleep(data->cas_prichodu);
+    printf("Zakaznik %u je v bare!\n",data->id);
+    pthread_mutex_lock(&data->pult->mutex);
+    while(!data->pult->pult) {
+        pthread_cond_wait(&data->pult->odober,&data->pult->mutex);
+    }
+    data->pult->pult=false;
+    pthread_cond_signal(&data->pult->pridaj);
+    pthread_mutex_unlock(&data->pult->mutex);
+    printf("Zakaznik %u si dava svoj drink a konci!\n",data->id);
+    return NULL;
+}
+
+int main(int argc, char * argv[]) {
+    srand(time(NULL));
+    PULT pult;
+    pult.pult=false;
+    pthread_mutex_init(&pult.mutex, NULL);
+    pthread_cond_init(&pult.pridaj,NULL);
+    pthread_cond_init(&pult.odober,NULL);
+    BARMAN dataB = {&pult, 10};
+    if(argc > 1 ) {
+        dataB.pocet_drinkov = atoi(argv[1]);
+    }
+    pthread_t barman;
+    pthread_t zakaznici[dataB.pocet_drinkov];
+    ZAKAZNIK dataZ[dataB.pocet_drinkov];
+    pthread_create(&barman, NULL, barman_fun, &dataB);
+    for (unsigned int i = 0; i < dataB.pocet_drinkov; ++i) {
+        dataZ[i].id=i+1;
+        dataZ[i].pult=&pult;
+        dataZ[i].cas_prichodu= rand() % 8 + 1;
+        pthread_create(&zakaznici[i], NULL, zakaznik_fun, &dataZ[i]);
+    }
+    printf("Bar otvoreny!\n");
+    pthread_join(barman,NULL);
+    for (int i = 0; i < dataB.pocet_drinkov; ++i) {
+        pthread_join(zakaznici[i],NULL);
+    }
+    printf("Bar zatvoreny!\n");
+    pthread_mutex_destroy(&pult.mutex);
+    pthread_cond_destroy(&pult.pridaj);
+    pthread_cond_destroy(&pult.odober);
+    return 0;
+}
--- u15_zber_caju.c
+++ u15_zber_caju.c
@@ -1,114 +1,114 @@
-//#include <stdio.h>
-//#include <stdbool.h>
-//#include <stdlib.h>
-//#include <pthread.h>
-//#include <unistd.h>
-//
-//typedef struct sklad {
-//    int kapacita;
-//    int pocetVSklade;
-//    _Bool *data;
-//} SKLAD;
-//
-//typedef struct data_plantaznici {
-//    int id;
-//    int trieda;
-//    SKLAD *sklad;
-//    int *plantaz;
-//    pthread_mutex_t *sklad_mutex;
-//    pthread_mutex_t *plantaz_mutex;
-//    pthread_cond_t *miesto_v_sklade;
-//    pthread_cond_t *nieco_v_sklade;
-//} PLANTAZNICI;
-//
-//typedef struct data_kontrolor {
-//    SKLAD *sklad;
-//    pthread_mutex_t *sklad_mutex;
-//    pthread_cond_t *miesto_v_sklade;
-//    pthread_cond_t *nieco_v_sklade;
-//} KONTROLOR;
-//
-//void *zbieraj_caj(void *data) {
-//    PLANTAZNICI *d = (PLANTAZNICI *)data;
-//    printf("Plantaznik %d triedy %d zacina cinnost\n", d->id, d->trieda);
-//
-//    // Kym je nieco na plantazi budeme opakovat nasledujuce
-//    pthread_mutex_lock(d->plantaz_mutex);
-//    while (d->plantaz > 0) {
-//        pthread_mutex_unlock(d->plantaz_mutex);
-//
-//        // Cesta z pristavu na plantaz
-//        printf("Plantaznik %d triedy %d ide na plantaz\n", d->id, d->trieda);
-//        if(d->trieda == 1) sleep(4);
-//        if(d->trieda == 2) sleep(6);
-//        if(d->trieda == 3) sleep(3);
-//
-//        // Zber caju na plantazi
-//        printf("Plantaznik %d triedy %d prisiel na plantaz a ide zberat caj\n", d->id, d->trieda);
-//        pthread_mutex_lock(d->plantaz_mutex);
-//        if(d->plantaz <= 0) break;
-//        if(d->trieda == 1) usleep(1000);
-//        if(d->trieda == 2) usleep(2000);
-//        if(d->trieda == 3) usleep(1500);
-//        --d->plantaz;
-//        pthread_mutex_unlock(d->plantaz_mutex);
-//
-//        // Cesta spat do pristavu
-//        printf("Plantaznik %d triedy %d zbieral caj a ide s cajom do pristavu\n", d->id, d->trieda);
-//        if(d->trieda == 1) sleep(2);
-//        if(d->trieda == 2) sleep(3);
-//        if(d->trieda == 3) sleep(3);
-//        pthread_mutex_lock(d->sklad_mutex);
-//        while (d->sklad->pocetVSklade >= d->sklad->kapacita) {
-//            pthread_cond_wait(d->miesto_v_sklade, d->sklad_mutex);
-//        }
-//
-//        // Vylozime dodavku
-//        if(d->trieda == 3) d->sklad->data[d->sklad->pocetVSklade] = false;
-//        else d->sklad->data[d->sklad->pocetVSklade] = true;
-//        ++d->sklad->pocetVSklade;
-//        pthread_mutex_unlock(d->sklad_mutex);
-//        pthread_cond_signal(d->nieco_v_sklade);
-//        printf("Plantaznik %d triedy %d odovzdal dodavku\n", d->id, d->trieda);
-//        pthread_mutex_lock(d->plantaz_mutex);
-//    }
-//    pthread_mutex_unlock(d->plantaz_mutex);
-//}
-//
-//void *kontroluj(void *data) {
-//    KONTROLOR *d = (KONTROLOR *)data;
-//    int pocet_vhodnych = 0, pocet_nevhodnych = 0;
-//
-//    //kym neskontroloval vsetky dodavky opakuj
-//    for (int i = 0; i < 60; ++i) {
-//        //ak je sklad prazdny, caka
-//        printf("Kontrolor ide do skladu\n");
-//        pthread_mutex_lock(d->sklad_mutex);
-//        while(d->sklad->pocetVSklade <= 0) {
-//            pthread_cond_wait(d->nieco_v_sklade, d->sklad_mutex);
-//        }
-//        //vyberie dodavku zo skladu
-//        --d->sklad->pocetVSklade;
-//        _Bool dodavka = d->sklad->data[d->sklad->pocetVSklade];
-//        pthread_mutex_unlock(d->sklad_mutex);
-//        pthread_cond_broadcast(d->miesto_v_sklade);
-//        printf("Kontrolor prebral dodavku a ide kontrolovat\n");
-//
-//        //skontroluje dodavku
-//        if(dodavka) {
-//            sleep(2);
-//            ++pocet_vhodnych;
-//        }
-//        else {
-//            sleep(4);
-//            ++pocet_nevhodnych;
-//        }
-//
-//        //zapise si vysledok a vypise ho
-//        printf("Kontrolor spracoval dodavku %d, pomer vhodnych a nevhodnych: %lf\n", i, (double)pocet_nevhodnych/pocet_vhodnych);
-//    }
-//}
-//
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+typedef struct sklad {
+    int kapacita;
+    int pocetVSklade;
+    _Bool *data;
+} SKLAD;
+
+typedef struct data_plantaznici {
+    int id;
+    int trieda;
+    SKLAD *sklad;
+    int *plantaz;
+    pthread_mutex_t *sklad_mutex;
+    pthread_mutex_t *plantaz_mutex;
+    pthread_cond_t *miesto_v_sklade;
+    pthread_cond_t *nieco_v_sklade;
+} PLANTAZNICI;
+
+typedef struct data_kontrolor {
+    SKLAD *sklad;
+    pthread_mutex_t *sklad_mutex;
+    pthread_cond_t *miesto_v_sklade;
+    pthread_cond_t *nieco_v_sklade;
+} KONTROLOR;
+
+void *zbieraj_caj(void *data) {
+    PLANTAZNICI *d = (PLANTAZNICI *)data;
+    printf("Plantaznik %d triedy %d zacina cinnost\n", d->id, d->trieda);
+
+    // Kym je nieco na plantazi budeme opakovat nasledujuce
+    pthread_mutex_lock(d->plantaz_mutex);
+    while (d->plantaz > 0) {
+        pthread_mutex_unlock(d->plantaz_mutex);
+
+        // Cesta z pristavu na plantaz
+        printf("Plantaznik %d triedy %d ide na plantaz\n", d->id, d->trieda);
+        if(d->trieda == 1) sleep(4);
+        if(d->trieda == 2) sleep(6);
+        if(d->trieda == 3) sleep(3);
+
+        // Zber caju na plantazi
+        printf("Plantaznik %d triedy %d prisiel na plantaz a ide zberat caj\n", d->id, d->trieda);
+        pthread_mutex_lock(d->plantaz_mutex);
+        if(d->plantaz <= 0) break;
+        if(d->trieda == 1) usleep(1000);
+        if(d->trieda == 2) usleep(2000);
+        if(d->trieda == 3) usleep(1500);
+        --d->plantaz;
+        pthread_mutex_unlock(d->plantaz_mutex);
+
+        // Cesta spat do pristavu
+        printf("Plantaznik %d triedy %d zbieral caj a ide s cajom do pristavu\n", d->id, d->trieda);
+        if(d->trieda == 1) sleep(2);
+        if(d->trieda == 2) sleep(3);
+        if(d->trieda == 3) sleep(3);
+        pthread_mutex_lock(d->sklad_mutex);
+        while (d->sklad->pocetVSklade >= d->sklad->kapacita) {
+            pthread_cond_wait(d->miesto_v_sklade, d->sklad_mutex);
+        }
+
+        // Vylozime dodavku
+        if(d->trieda == 3) d->sklad->data[d->sklad->pocetVSklade] = false;
+        else d->sklad->data[d->sklad->pocetVSklade] = true;
+        ++d->sklad->pocetVSklade;
+        pthread_mutex_unlock(d->sklad_mutex);
+        pthread_cond_signal(d->nieco_v_sklade);
+        printf("Plantaznik %d triedy %d odovzdal dodavku\n", d->id, d->trieda);
+        pthread_mutex_lock(d->plantaz_mutex);
+    }
+    pthread_mutex_unlock(d->plantaz_mutex);
+}
+
+void *kontroluj(void *data) {
+    KONTROLOR *d = (KONTROLOR *)data;
+    int pocet_vhodnych = 0, pocet_nevhodnych = 0;
+
+    //kym neskontroloval vsetky dodavky opakuj
+    for (int i = 0; i < 60; ++i) {
+        //ak je sklad prazdny, caka
+        printf("Kontrolor ide do skladu\n");
+        pthread_mutex_lock(d->sklad_mutex);
+        while(d->sklad->pocetVSklade <= 0) {
+            pthread_cond_wait(d->nieco_v_sklade, d->sklad_mutex);
+        }
+        //vyberie dodavku zo skladu
+        --d->sklad->pocetVSklade;
+        _Bool dodavka = d->sklad->data[d->sklad->pocetVSklade];
+        pthread_mutex_unlock(d->sklad_mutex);
+        pthread_cond_broadcast(d->miesto_v_sklade);
+        printf("Kontrolor prebral dodavku a ide kontrolovat\n");
+
+        //skontroluje dodavku
+        if(dodavka) {
+            sleep(2);
+            ++pocet_vhodnych;
+        }
+        else {
+            sleep(4);
+            ++pocet_nevhodnych;
+        }
+
+        //zapise si vysledok a vypise ho
+        printf("Kontrolor spracoval dodavku %d, pomer vhodnych a nevhodnych: %lf\n", i, (double)pocet_nevhodnych/pocet_vhodnych);
+    }
+}
+
 //int main(int args, char **argv) {
 //    srand(time(NULL));
 //    int pocet_plantaznikov = 10;
