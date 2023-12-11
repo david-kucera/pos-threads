d25d217d27cb51cc0d4da37efeef358fc7a38bed
David Kučera <kucera.david2002@gmail.com>
2023-12-11 15:33:24 +0100

    ref2 du_rabcan2023

--- du_rabcan2023.c
+++ du_rabcan2023.c
@@ -4,7 +4,7 @@
 #include <stdbool.h>
 #include <unistd.h>
 
-int pocet_autobusov, pocet_cestujucich;
+int pocet_cestujucich;
 
 typedef struct zastavka {
     int pocet_cestujucich;
@@ -22,10 +22,6 @@ typedef struct autobus {
     ZASTAVKA * zastavka;
 } AUTOBUS;
 
-typedef struct opravar {
-    ZASTAVKA * zastavka;
-} OPRAVAR;
-
 void init_zastavka(ZASTAVKA * zastavka) {
     zastavka->pocet_cestujucich = pocet_cestujucich;
     zastavka->obsadena = false;
@@ -42,26 +38,20 @@ void destroy_zastavka(ZASTAVKA * zastavka) {
     pthread_cond_destroy(&zastavka->je_obsadena);
 }
 
-void * opravar_fun(void * arg) {
-    return NULL;
-}
-
 void * autobus_fun(void * arg) {
     AUTOBUS * data = (AUTOBUS *)arg;
 
     while (data->zastavka->pocet_cestujucich != 0) {
         data->pocet_cestujucich = 0;
-        data->id++;
 
         pthread_mutex_lock(&data->zastavka->mutex);
-        while (!data->zastavka->je_obsadena) {
+        while (data->zastavka->obsadena) {
             printf("Zastavka je obsadena! Autobus %i caka na uvolnenie zastavky.\n", data->id);
             pthread_cond_wait(&data->zastavka->je_obsadena, &data->zastavka->mutex);
         }
 
-
         printf("Autobus %i prichadza na zastavku. Jeho kapacita je %i osob.\n", data->id, data->kapacita);
-        data->zastavka->je_obsadena = true;
+        data->zastavka->obsadena = true;
         if (data->zastavka->pocet_cestujucich > data->kapacita) {
             data->zastavka->pocet_cestujucich -= data->kapacita;
             data->pocet_cestujucich = data->kapacita;
@@ -85,6 +75,7 @@ void * autobus_fun(void * arg) {
             sleep(cas);
         }
         printf("Vsetci cestujuci nastupili do autobusu %i!\n", data->id);
+        data->zastavka->obsadena = false;
         pthread_cond_signal(&data->zastavka->je_prazdna);
         pthread_mutex_unlock(&data->zastavka->mutex);
 
@@ -93,41 +84,41 @@ void * autobus_fun(void * arg) {
         sleep(cas);
         printf("Autobus %i prisiel do finalnej destinacie!\n", data->id);
     }
-
-    printf("Autobusy odviezli vsetkych cestujucich!\n");
     return NULL;
 }
 
 int main(int argc, char * argv[]) {
     srand(time(NULL));
-    // Pocet autobusov, ako aj pocet cestujucich na zastavke budu ako argumenty programu
+
+    // Pocet autobusvo mozeme ziskat z argumentu programu, inak ich bude 40
     if (argc != 3) {
-        pocet_autobusov = 3;
-        pocet_cestujucich = 100;
+        pocet_cestujucich = 40;
     } else {
-        pocet_autobusov = atoi(argv[1]);
-        pocet_cestujucich = atoi(argv[2]);
+        pocet_cestujucich = atoi(argv[1]);
     }
 
     printf("SPUSTA SA PROGRAM!\n");
 
+    // Predpokladame, ze kapacita autobusu je 20 osob
+
     ZASTAVKA zastavka;
     init_zastavka(&zastavka);
 
-    AUTOBUS autobus_data;
-    pthread_t autobusy;
-    autobus_data.id = 0;
-    autobus_data.pocet_cestujucich = 0;
-    autobus_data.kapacita = 40;
-    autobus_data.zastavka = &zastavka;
-    pthread_create(&autobusy, NULL, autobus_fun, &autobus_data);
+    int pocet_autobusov = pocet_cestujucich / 20;
 
-//    OPRAVAR opravar;
-//    pthread_t opravart;
-//    pthread_create(&opravart, NULL, opravar_fun, &opravar);
+    AUTOBUS autobus_data[pocet_autobusov];
+    pthread_t autobusy[pocet_autobusov];
+    for (int i = 0; i < pocet_autobusov; ++i) {
+        autobus_data[i].id = i + 1;
+        autobus_data[i].pocet_cestujucich = 0;
+        autobus_data[i].kapacita = 20;
+        autobus_data[i].zastavka = &zastavka;
+        pthread_create(&autobusy[i], NULL, autobus_fun, &autobus_data[i]);
+    }
 
-//    pthread_join(opravart, NULL);
-    pthread_join(autobusy, NULL);
+    for (int i = 0; i < pocet_autobusov; ++i) {
+        pthread_join(autobusy[i], NULL);
+    }
 
     printf("Vsetci cestujuci boli odvezeni na zapas! Program konci.\n");
     destroy_zastavka(&zastavka);
